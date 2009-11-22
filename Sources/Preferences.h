8943c12490dd24087dc7c093a434a6236f722e1b
Adrian Stutz <adrian@sttz.ch>
2009-11-22 19:48:31 +0000

    Use constants for preferences keys instead of duplicating strings all over the place.
    git-svn-id: svn://mplayerosx.ch/mplayerosx/trunk@177 6577e75b-6453-0410-a6fa-8f9c828ad7c1

---
+++ Sources/Preferences.h
@@ -0,0 +1,66 @@
+/*
+ *  Preferences.h
+ *  MPlayer OSX Extended
+ *
+ *  Created by Adrian on 20.11.09.
+ *  Copyright 2009 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#define PREFS [NSUserDefaults standardUserDefaults]
+
+extern NSString* const MPEAdvancedOptions;
+extern NSString* const MPEAnimateInterfaceTransitions;
+extern NSString* const MPEAspectRatio;
+extern NSString* const MPEBS2BFilter;
+extern NSString* const MPEBlackOutOtherScreensInFullscreen;
+extern NSString* const MPECacheSizeInMB;
+extern NSString* const MPECheckForUpdates;
+extern NSString* const MPECheckForUpdatesIncludesPrereleases;
+extern NSString* const MPECustomAspectRatio;
+extern NSString* const MPECustomSizeInPx;
+extern NSString* const MPEDefaultAudioLanguages;
+extern NSString* const MPEDefaultDirectory;
+extern NSString* const MPEDefaultSubtitleLanguages;
+extern NSString* const MPEDeinterlaceFilter;
+extern NSString* const MPEDisplaySize;
+extern NSString* const MPEDropFrames;
+extern NSString* const MPEEnableAudio;
+extern NSString* const MPEEnableVideo;
+extern NSString* const MPEFastDecoding;
+extern NSString* const MPEFlipDisplayHorizontally;
+extern NSString* const MPEFlipDisplayVertically;
+extern NSString* const MPEFont;
+extern NSString* const MPEFontStyle;
+extern NSString* const MPEFullscreenDisplayNumber;
+extern NSString* const MPEGoToFullscreenOn;
+extern NSString* const MPEGuessTextEncoding;
+extern NSString* const MPEHRTFFilter;
+extern NSString* const MPEHardwareAC3Passthrough;
+extern NSString* const MPEHardwareDTSPassthrough;
+extern NSString* const MPEKaraokeFilter;
+extern NSString* const MPELoadEmbeddedFonts;
+extern NSString* const MPEOSDLevel;
+extern NSString* const MPEOSDScale;
+extern NSString* const MPEOverrideAudioCodecs;
+extern NSString* const MPEOverrideVideoCodecs;
+extern NSString* const MPEPostprocessingFilter;
+extern NSString* const MPERenderSubtitlesFirst;
+extern NSString* const MPEScreenshotSaveLocation;
+extern NSString* const MPEStartPlaybackDisplayType;
+extern NSString* const MPESubtitleBorderColor;
+extern NSString* const MPESubtitleScale;
+extern NSString* const MPESubtitleTextColor;
+extern NSString* const MPETextEncoding;
+extern NSString* const MPEWindowOnTopMode;
+
+extern NSString* const MPECustomAspectRatioStringKey;
+extern NSString* const MPECustomAspectRatioValueKey;
+
+extern int const MPEStartPlaybackDisplayTypeWindow;
+extern int const MPEStartPlaybackDisplayTypeFullscreen;
+extern int const MPEStartPlaybackDisplayTypeDesktop;
+
+extern int const MPEWindowOnTopModeNever; 
+extern int const MPEWindowOnTopModeWhilePlaying;
+extern int const MPEWindowOnTopModeAlways;
---
+++ Sources/Preferences.m
@@ -0,0 +1,65 @@
+//
+//  Preferences.m
+//  MPlayer OSX Extended
+//
+//  Created by Adrian on 20.11.09.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#include "Preferences.h"
+
+NSString* const MPEAdvancedOptions                    = @"MPEAdvancedOptions";
+NSString* const MPEAnimateInterfaceTransitions        = @"MPEAnimateInterfaceTransitions";
+NSString* const MPEAspectRatio                        = @"MPEAspectRatio";
+NSString* const MPEBS2BFilter                         = @"MPEBS2BFilter";
+NSString* const MPEBlackOutOtherScreensInFullscreen   = @"MPEBlackOutOtherScreensInFullscreen";
+NSString* const MPECacheSizeInMB                      = @"MPECacheSizeInMB";
+NSString* const MPECheckForUpdates                    = @"MPECheckForUpdates";
+NSString* const MPECheckForUpdatesIncludesPrereleases = @"MPECheckForUpdatesIncludesPrereleases";
+NSString* const MPECustomAspectRatio                  = @"MPECustomAspectRatio";
+NSString* const MPECustomSizeInPx                     = @"MPECustomSizeInPx";
+NSString* const MPEDefaultAudioLanguages              = @"MPEDefaultAudioLanguages";
+NSString* const MPEDefaultDirectory                   = @"MPEDefaultDirectory";
+NSString* const MPEDefaultSubtitleLanguages           = @"MPEDefaultSubtitleLanguages";
+NSString* const MPEDeinterlaceFilter                  = @"MPEDeinterlaceFilter";
+NSString* const MPEDisplaySize                        = @"MPEDisplaySize";
+NSString* const MPEDropFrames                         = @"MPEDropFrames";
+NSString* const MPEEnableAudio                        = @"MPEEnableAudio";
+NSString* const MPEEnableVideo                        = @"MPEEnableVideo";
+NSString* const MPEFastDecoding                       = @"MPEFastDecoding";
+NSString* const MPEFlipDisplayHorizontally            = @"MPEFlipDisplayHorizontally";
+NSString* const MPEFlipDisplayVertically              = @"MPEFlipDisplayVertically";
+NSString* const MPEFont                               = @"MPEFont";
+NSString* const MPEFontStyle                          = @"MPEFontStyle";
+NSString* const MPEFullscreenDisplayNumber            = @"MPEFullscreenDisplayNumber";
+NSString* const MPEGoToFullscreenOn                   = @"MPEGoToFullscreenOn";
+NSString* const MPEGuessTextEncoding                  = @"MPEGuessTextEncoding";
+NSString* const MPEHRTFFilter                         = @"MPEHRTFFilter";
+NSString* const MPEHardwareAC3Passthrough             = @"MPEHardwareAC3Passthrough";
+NSString* const MPEHardwareDTSPassthrough             = @"MPEHardwareDTSPassthrough";
+NSString* const MPEKaraokeFilter                      = @"MPEKaraokeFilter";
+NSString* const MPELoadEmbeddedFonts                  = @"MPELoadEmbeddedFonts";
+NSString* const MPEOSDLevel                           = @"MPEOSDLevel";
+NSString* const MPEOSDScale                           = @"MPEOSDScale";
+NSString* const MPEOverrideAudioCodecs                = @"MPEOverrideAudioCodecs";
+NSString* const MPEOverrideVideoCodecs                = @"MPEOverrideVideoCodecs";
+NSString* const MPEPostprocessingFilter               = @"MPEPostprocessingFilter";
+NSString* const MPERenderSubtitlesFirst               = @"MPERenderSubtitlesFirst";
+NSString* const MPEScreenshotSaveLocation             = @"MPEScreenshotSaveLocation";
+NSString* const MPEStartPlaybackDisplayType           = @"MPEStartPlaybackDisplayType";
+NSString* const MPESubtitleBorderColor                = @"MPESubtitleBorderColor";
+NSString* const MPESubtitleScale                      = @"MPESubtitleScale";
+NSString* const MPESubtitleTextColor                  = @"MPESubtitleTextColor";
+NSString* const MPETextEncoding                       = @"MPETextEncoding";
+NSString* const MPEWindowOnTopMode                    = @"MPEWindowOnTopMode";
+
+NSString* const MPECustomAspectRatioStringKey         = @"MPECustomAspectRatioString";
+NSString* const MPECustomAspectRatioValueKey          = @"MPECustomAspectRatioValue";
+
+int const MPEStartPlaybackDisplayTypeWindow           = 0;
+int const MPEStartPlaybackDisplayTypeFullscreen       = 1;
+int const MPEStartPlaybackDisplayTypeDesktop          = 2;
+
+int const MPEWindowOnTopModeNever                     = 0;
+int const MPEWindowOnTopModeWhilePlaying              = 1;
+int const MPEWindowOnTopModeAlways                    = 2;
